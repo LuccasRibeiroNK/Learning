3cc881d5fe8369fc41584e87b8344a5ffecd9394
luccasribeironk <luckgl@hotmail.com>
2021-06-09 14:54:06 -0300

    First Push

---
+++ JavaScript/myDogs.js
@@ -0,0 +1,40 @@
+/* var dogs = {
+    Fido: "Mutt", Hunter: "Doberman", Snoopie: "Beagle"
+};
+var myDog = "Fido";
+var myBreed = dogs[myDog];
+console.log(myBreed); */
+
+/* var someObj = {
+    propName: "John"
+};
+function propPrefix(str){
+    var s = "prop";
+    return s + str;
+}
+var someProp = propPrefix("Name");
+console.log(someObj[someProp]); */
+
+/* var ourDog = {
+    "name": "Camper",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["everything!"]
+};
+
+ourDog.name = "Happy Camper"
+ourDog.legs = 3
+console.log(ourDog.name)
+console.log(ourDog.legs) */
+
+var myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  
+  myDog["bark"] = "woof"
+  myDog.eat = "carne"
+
+  console.log(myDog)
